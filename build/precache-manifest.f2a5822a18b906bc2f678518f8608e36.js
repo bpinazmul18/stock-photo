04a4f4a738c9418ed801af70869d36ab0ac40d40
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-05-03 04:29:57 +0600

    add build command

---
+++ build/precache-manifest.f2a5822a18b906bc2f678518f8608e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5090e309c8763320626934c62e377629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0437085695f3d3c9e16",
+    "url": "/static/css/main.1a574bb5.chunk.css"
+  },
+  {
+    "revision": "79d1e31031c34bb12d15",
+    "url": "/static/js/2.6b3cad7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6b3cad7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0437085695f3d3c9e16",
+    "url": "/static/js/main.268c41bc.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
